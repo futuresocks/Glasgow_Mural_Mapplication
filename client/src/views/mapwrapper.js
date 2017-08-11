4bdc67a546669a1d29257edffae62b939d869ba7
John Allan <johnjamesharvey@gmail.com>
2017-08-11 10:44:07 +0100

    mapwrapper.js file and ui.js

---
+++ client/src/views/mapwrapper.js
@@ -0,0 +1,13 @@
+var MapWrapper = function(container, center, zoom){
+  this.googleMap = new google.maps.Map(
+    container, {
+      center: center,
+      zoom: zoom
+    });
+};
+
+MapWrapper.prototype.setCenter = function(coords){
+  this.googleMap.setCenter(coords);
+}
+
+module.exports = MapWrapper;
