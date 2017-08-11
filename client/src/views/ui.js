5a473394f3e31cfd9d02a11c27eaf00b3c5d9d38
Crawford Davidson <crawforddavidson@gmail.com>
2017-08-11 14:13:15 +0100

    ui, models and app.js updated. can now access mural api from UI

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,30 +1,28 @@
 var MapWrapper = require('./mapWrapper.js');
+var Murals = require('../models/murals')
 
-var UI = function(murals){
-    this.render(murals);
+var UI = function(){
+  var murals = new Murals();
+  murals.all(function(result){
+    UI.prototype.render(result);
+  });
 }
 
 UI.prototype = {
-    render: function(murals){
-      console.log(murals);
-      murals.forEach(function(mural){
+  render: function(murals){
 
+    murals.forEach(function(mural){
+      console.log(mural.title)
+    })
 
+    
+    // mapDiv.style.height = '200px';
+    // mapDiv.style.width = '200px';
 
-
-
-        mapDiv.style.height = '200px';
-        mapDiv.style.width = '200px';
-
-        var center = {lat: mural.latlng[0], lng: mural.latlng[1]};
-        var zoom = 10;
-        var mainMap = new MapWrapper(mapDiv, center, zoom);
-      })
-
-    }
-
-
-
+    // var center = {lat: mural.latlng[0], lng: mural.latlng[1]};
+    // var zoom = 10;
+    // var mainMap = new MapWrapper(mapDiv, center, zoom);
+  }
 }
 
 module.exports = UI;
