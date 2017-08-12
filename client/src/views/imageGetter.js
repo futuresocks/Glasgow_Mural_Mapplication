a3e4c6b497e96599425abfa0fc296cc60240fa27
Colin Bell <colin@dogoodercomics.co.uk>
2017-08-12 16:39:58 +0100

    imageGetter returning all flickr photos by string search

--- client/src/views/imageGetter.js
+++ client/src/views/imageGetter.js
@@ -1,23 +1,35 @@
 var ImageGetter = function(){
-  var apiKey = "e83c635ab5aca5ad0990f1d779a36309";
 
-}
-
-ImageGetter.prototype = {
- 
-  getPhoto: function(string, callback){
-    var request = new XMLHttpRequest();
-    request.open("GET", "http://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=" + apiKey + "&tags=" + string + "&format=json&nojsoncallback=1");
-    request.addEventListener("load", callback);
-    request.send();
-  }
-
-  firstPhoto: function(category){
-    if(this.status !==200) return;
-    var jsonString = this.responseText;
-    var data = JSON.parse(jsonString);
-    console.log(data);
   }    
+
+  ImageGetter.prototype = {
+    makeRequest: function(string, callback){
+      var request = new XMLHttpRequest();
+      request.open('GET', "https://api.flickr.com/services/rest/?&method=flickr.photos.search&api_key=e83c635ab5aca5ad0990f1d779a36309&tags=" + string + "&format=json&nojsoncallback=1");
+      request.addEventListener('load', callback);
+      request.send();
+    },
+
+    allPhotos: function(string){
+      this.makeRequest(string, function(){
+        if(this.status !== 200)return;
+        var jsonString = this.responseText;
+        var data = JSON.parse(jsonString);
+        console.log(data.photos.photo[0]);
+        // callback(murals);
+      })
+    },
+
+    firstPhoto: function(string){
+      this.makeRequest(string, function(){
+        if(this.status !== 200)return;
+        var jsonString = this.responseText;
+        var data = JSON.parse(jsonString);
+        var photo = data.photos.photo[0];
+        return photo;
+      })
+    },
+
 
   // photos.photo[0].
 
