b7f65b42da05554f37cd29f9807c83663d40a7cd
John Allan <johnjamesharvey@gmail.com>
2017-08-14 20:52:02 +0100

    about page styling, seprate css pages

--- client/build/animation.js
+++ client/build/animation.js
@@ -1,12 +1,12 @@
 var left = 0;
 var right = 0;
-var top = 0;
+
 
   function frame() {
     var element = document.querySelector('.item-right');
     left += 1;
     element. style.left = left + 'px';
-    if (left >= 800) {
+    if (left >= 150) {
       clearInterval(id)
     }
   }
@@ -16,15 +16,22 @@ var top = 0;
     var element = document.querySelector('.item-left');
     right += 1;
     element. style.right = right + 'px';
-    if (right >= 300) {
+    if (right >= 150) {
       clearInterval(idRight)
     }
   }
 
 
-  var id = setInterval(frame, 45)
-  var idRight = setInterval(frameLeft, 50)
+  var id = setInterval(frame, 120)
+  var idRight = setInterval(frameLeft, 120)
 
 
-
+  $(document).ready(function() {
+    $('#play-video').on('click', function(ev) {
+   
+      $("#video")[0].src += "&autoplay=1";
+      ev.preventDefault();
+   
+    });
+  });
   
