266465d74507348c1f4debc1c7153ac8e34b49ac
John Allan <johnjamesharvey@gmail.com>
2017-08-14 15:44:43 +0100

    styling buttons

---
+++ client/build/animation.js
@@ -0,0 +1,28 @@
+var left = 0;
+var right = 0;
+var top = 0;
+
+  function frame() {
+    var element = document.querySelector('.item-right');
+    left += 1;
+    element. style.left = left + 'px';
+    if (left >= 800) {
+      clearInterval(id)
+    }
+  }
+
+
+  function frameLeft() {
+    var element = document.querySelector('.item-left');
+    right += 1;
+    element. style.right = right + 'px';
+    if (right >= 800) {
+      clearInterval(idRight)
+    }
+  }
+
+  var id = setInterval(frame, 5)
+  var idRight = setInterval(frameLeft, 5)
+
+
+  
